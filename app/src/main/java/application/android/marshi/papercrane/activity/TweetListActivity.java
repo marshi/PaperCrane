e95171e012cd2ecb1315964de39ae806a493520f
marshi <mashi3648@gmail.com>
2016-04-08 02:30:35 +0900

    DrawerLayoutのなかにCoordinatorLayoutをいれてみる

--- app/src/main/java/application/android/marshi/papercrane/activity/TweetListActivity.java
+++ app/src/main/java/application/android/marshi/papercrane/activity/TweetListActivity.java
@@ -1,9 +1,7 @@
 package application.android.marshi.papercrane.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -27,11 +25,11 @@ public class TweetListActivity extends AppCompatActivity
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(view ->
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG
-				).setAction("Action", null).show()
-		);
+//		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//		fab.setOnClickListener(view ->
+//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG
+//				).setAction("Action", null).show()
+//		);
 
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
