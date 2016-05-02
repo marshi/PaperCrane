2b2cb002d354b4e012192b17a5a0c42e90badf54
marshi <mashi3648@gmail.com>
2016-05-02 21:19:20 +0900

    無限スクロール実装

--- app/src/main/java/application/android/marshi/papercrane/presenter/twitter/TimelinePresenter.java
+++ app/src/main/java/application/android/marshi/papercrane/presenter/twitter/TimelinePresenter.java
@@ -3,10 +3,13 @@ package application.android.marshi.papercrane.presenter.twitter;
 import application.android.marshi.papercrane.domain.model.TweetItem;
 import application.android.marshi.papercrane.domain.usecase.timeline.GetTimelineUseCase;
 import application.android.marshi.papercrane.presenter.Presenter;
+import twitter4j.Paging;
 import twitter4j.auth.AccessToken;
 
 import javax.inject.Inject;
 import java.util.List;
+
+import static application.android.marshi.papercrane.domain.usecase.timeline.GetTimelineUseCase.TimelineRequest;
 
 /**
  * @author marshi on 2016/04/23.
@@ -20,7 +23,14 @@ public class TimelinePresenter extends Presenter {
 	GetTimelineUseCase getTimelineUseCase;
 
 	public List<TweetItem> getTweetItems(AccessToken accessToken) {
-		getTimelineUseCase.start(accessToken);
+		Paging paging = new Paging(1);
+		getTimelineUseCase.start(new TimelineRequest(accessToken, paging));
+		return null;
+	}
+
+	public List<TweetItem> getTweetItems(AccessToken accessToken, long maxId) {
+		Paging paging = new Paging().maxId(maxId - 1).count(20);
+		getTimelineUseCase.start(new TimelineRequest(accessToken, paging));
 		return null;
 	}
 
--- app/src/main/java/application/android/marshi/papercrane/repository/TweetRepository.java
+++ app/src/main/java/application/android/marshi/papercrane/repository/TweetRepository.java
@@ -1,12 +1,13 @@
 package application.android.marshi.papercrane.repository;
 
-import android.util.Log;
 import application.android.marshi.papercrane.TwitterClient;
 import application.android.marshi.papercrane.domain.model.TweetItem;
 import lombok.Data;
+import twitter4j.Paging;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.User;
 import twitter4j.auth.AccessToken;
 
@@ -23,19 +24,14 @@ public class TweetRepository {
 	@Inject
 	public TweetRepository(){}
 
-	public List<TweetItem> getTweetItemList(AccessToken accessToken) {
-		try {
-			Twitter twitter = TwitterClient.getInstance(accessToken);
-			ResponseList<Status> statuses =  twitter.getHomeTimeline();
-			List<TweetItem> tweetItemList = new ArrayList<>();
-			for (Status status : statuses) {
-				tweetItemList.add(convertFrom(status));
-			}
-			return tweetItemList;
-		} catch (Exception e) {
-			Log.e("tag", e.toString());
+	public List<TweetItem> getTweetItemList(AccessToken accessToken, Paging paging) throws TwitterException {
+		Twitter twitter = TwitterClient.getInstance(accessToken);
+		ResponseList<Status> statuses =  twitter.getHomeTimeline(paging);
+		List<TweetItem> tweetItemList = new ArrayList<>();
+		for (Status status : statuses) {
+			tweetItemList.add(convertFrom(status));
 		}
-		return null;
+		return tweetItemList;
 	}
 
 	private TweetItem convertFrom(Status status) {
