0faa44c75c22c7cde527cb8acc8df52303c3d0f8
marshi <mashi3648@gmail.com>
2016-08-07 16:00:51 +0900

    一応fav機能作った

--- app/src/main/java/application/android/marshi/papercrane/presenter/TweetDetailPresenter.java
+++ app/src/main/java/application/android/marshi/papercrane/presenter/TweetDetailPresenter.java
@@ -6,7 +6,6 @@ import application.android.marshi.papercrane.domain.model.TweetItem;
 import application.android.marshi.papercrane.service.ToastService;
 import application.android.marshi.papercrane.service.auth.AccessTokenService;
 import application.android.marshi.papercrane.service.twitter.TwitterFavService;
-import rx.functions.Action1;
 
 import javax.inject.Inject;
 
@@ -30,27 +29,21 @@ public class TweetDetailPresenter {
 	public TweetDetailPresenter() {}
 
 	public void fav(TweetItem tweetItem, ImageView favImageView) {
-		Action1<Boolean> onNext;
-		Action1<Throwable> onError;
 		if (tweetItem.isFav()) {
-			onNext = b -> {
-				if (b) { favImageView.setSelected(false); }
-			};
-			onError = t -> {
-				toastService.showToast("お気に入り解除に失敗しました", Toast.LENGTH_SHORT);
-				t.printStackTrace();
-			};
-			twitterFavService.removeFav(accessTokenService.getAccessToken(), tweetItem.getId(), onNext, onError);
-		} else {
-			onNext = b -> {
-				if (b) { favImageView.setSelected(true); }
-			};
-			onError = t -> {
+			twitterFavService.removeFav(accessTokenService.getAccessToken(), tweetItem.getId(),
+				b -> {if (b) { favImageView.setSelected(false); }},
+				t -> {
+					toastService.showToast("お気に入り解除に失敗しました", Toast.LENGTH_SHORT);
+					t.printStackTrace();
+				});
+			return;
+		}
+		twitterFavService.addFav(accessTokenService.getAccessToken(), tweetItem.getId(),
+			b -> { if (b) { favImageView.setSelected(true); }},
+			t -> {
 				toastService.showToast("お気に入りに失敗しました", Toast.LENGTH_SHORT);
 				t.printStackTrace();
-			};
-			twitterFavService.addFav(accessTokenService.getAccessToken(), tweetItem.getId(), onNext, onError);
-		}
-
+			}
+		);
 	}
 }
